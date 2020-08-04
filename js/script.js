2d11cbfc7a45a521359a0ab806ab85550b88e2fb
Akumu collins <akumucollins001@gmail.com>
2020-08-04 11:46:14 +0300

    created our porfolio section and styled and a hover function

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 /*Navigation Function */
 $(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-      theme: "minimal",
-    });
-  
-    $("#dismiss, .overlay").on("click", function () {
-      // hide sidebar
-      $("#sidebar").removeClass("active");
-      // hide overlay
-      $(".overlay").removeClass("active");
-    });
-  
-    $("#sidebarCollapse").on("click", function () {
-      // open sidebar
-      $("#sidebar").addClass("active");
-      // fade in the overlay
-      $(".overlay").addClass("active");
-      $(".collapse.in").toggleClass("in");
-      $("a[aria-expanded=true]").attr("aria-expanded", "false");
-    });
+  $("#sidebar").mCustomScrollbar({
+    theme: "minimal",
   });
+
+  $("#dismiss, .overlay").on("click", function () {
+    // hide sidebar
+    $("#sidebar").removeClass("active");
+    // hide overlay
+    $(".overlay").removeClass("active");
+  });
+
+  $("#sidebarCollapse").on("click", function () {
+    // open sidebar
+    $("#sidebar").addClass("active");
+    // fade in the overlay
+    $(".overlay").addClass("active");
+    $(".collapse.in").toggleClass("in");
+    $("a[aria-expanded=true]").attr("aria-expanded", "false");
+  });
+});
+
+/*Hover Function */
+$(document).ready(function () {
+  $(".card").hover(function () {
+    $(this).children(".portfolio").fadeToggle("slow");
+  });
+});
